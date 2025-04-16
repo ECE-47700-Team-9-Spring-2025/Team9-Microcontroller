28717524c5670200bab1bc2252a0fa3a42cad4fe
John Stanwick <48192612+jwstanwick@users.noreply.github.com>
2025-04-16 16:13:47 -0400

    added control flow to calculate motor speed output
    added gngll parsing for non gnrmc messages

--- main/Core/Src/m8q.c
+++ main/Core/Src/m8q.c
@@ -139,3 +139,73 @@ bool M8Q_ParseGNRMC(const char* sentence, GPS_Data* data) {
     
     return false;
 }
+
+bool M8Q_ParseGNGLL(const char* sentence, GPS_Data* data) {
+    char *saveptr;
+    char *token = strtok_r((char*)sentence, ",", &saveptr);
+    int field = 0;
+    
+    while (token != NULL) {
+        switch(field) {
+            case 1: // Latitude
+                if (strlen(token) > 0) {
+                    data->latitude = atof(token);
+                }
+                break;
+                
+            case 2: // N/S
+                data->lat_direction = token[0];
+                break;
+                
+            case 3: // Longitude
+                if (strlen(token) > 0) {
+                    data->longitude = atof(token);
+                }
+                break;
+                
+            case 4: // E/W
+                data->lon_direction = token[0];
+                break;
+                
+            case 5: // Time
+                if (strlen(token) >= 6) {
+                    char time[3] = {0};
+                    // Hours
+                    strncpy(time, token, 2);
+                    data->hours = atoi(time);
+                    // Minutes
+                    strncpy(time, token + 2, 2);
+                    data->minutes = atoi(time);
+                    // Seconds
+                    strncpy(time, token + 4, 2);
+                    data->seconds = atoi(time);
+                }
+                break;
+                
+            case 6: // Status
+                data->fix_valid = (token[0] == 'A');
+                break;
+        }
+        token = strtok_r(NULL, ",", &saveptr);
+        field++;
+    }
+    
+    // Convert DDMM.MMMMM to decimal degrees
+    if (data->fix_valid) {
+        double lat_degrees = floor(data->latitude / 100);
+        double lat_minutes = data->latitude - (lat_degrees * 100);
+        data->latitude = lat_degrees + (lat_minutes / 60);
+        
+        double lon_degrees = floor(data->longitude / 100);
+        double lon_minutes = data->longitude - (lon_degrees * 100);
+        data->longitude = lon_degrees + (lon_minutes / 60);
+        
+        // Apply direction
+        if (data->lat_direction == 'S') data->latitude = -data->latitude;
+        if (data->lon_direction == 'W') data->longitude = -data->longitude;
+        
+        return true;
+    }
+    
+    return false;
+}
